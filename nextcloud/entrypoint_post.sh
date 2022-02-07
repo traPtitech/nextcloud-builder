58f2da49ef7e8e86b4a69c0c479fa17a989e8643
sapphi-red <green@sapphi.red>
2022-02-07 22:43:13 +0900

    Sync custom_apps always

--- nextcloud/entrypoint_post.sh
+++ nextcloud/entrypoint_post.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -eu
 
-# https://github.com/nextcloud/docker/blob/dcf058fb559e7918cd77f5f1bb0dfdbcee813571/23/fpm/entrypoint.sh#L9-L12
-# return true if specified directory is empty
-directory_empty() {
-    [ -z "$(ls -A "$1/")" ]
-}
-
 # https://github.com/nextcloud/docker/blob/dcf058fb559e7918cd77f5f1bb0dfdbcee813571/23/fpm/entrypoint.sh#L99-L103
 if [ "$(id -u)" = 0 ]; then
     rsync_options="-rlDog --chown www-data:root"
@@ -16,8 +10,6 @@ fi
 
 # https://github.com/nextcloud/docker/blob/dcf058fb559e7918cd77f5f1bb0dfdbcee813571/23/fpm/entrypoint.sh#L106-L110
 for dir in custom_apps; do
-    if [ ! -d "/var/www/html/$dir" ] || directory_empty "/var/www/html/$dir"; then
-        rsync $rsync_options --include "/$dir/" --exclude '/*' /usr/src/nextcloud/ /var/www/html/
-    fi
+    rsync $rsync_options --include "/$dir/" --exclude '/*' /usr/src/nextcloud/ /var/www/html/
 done
-echo Copied custom_apps
+echo Synced custom_apps
